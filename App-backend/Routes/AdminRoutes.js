8ebda3982e265b8f6c3b54c1a2d1080301d78181
Shefali360 <shefali.goyal@tothenew.com>
2020-06-30 12:53:31 +0530

    Deleted admin files and removed midware from getDept

--- App-backend/Routes/AdminRoutes.js
+++
@@ -1,9 +0,0 @@
-const admin = require("../../App-backend/Controller/AdminController");
-const midware = require("../Midwares/Midwares");
-const router = require("express").Router();
-
-router.post("/", midware.verifyTokenToGetUserData, admin.createAdmin);
-router.get("/",midware.verifyTokenToGetUserData, admin.getAdmin);
-router.delete("/:id", admin.delete);
-module.exports = router;
-
--- App-backend/Routes/DepartmentRoutes.js
+++ App-backend/Routes/DepartmentRoutes.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const midware = require("../Midwares/Midwares");
 
 router.post( "/",  midware.verifyTokenToGetUserData,midware.checkPrivileges("SuperAdmin"),dept.createDept);
-router.get("/",  midware.verifyTokenToGetUserData,midware.checkPrivileges("SuperAdmin"),dept.getDept);
+router.get("/",  midware.verifyTokenToGetUserData,dept.getDept);
 router.delete("/:id", midware.verifyTokenToGetUserData, midware.checkPrivileges("SuperAdmin"),dept.delete);
 
 module.exports = router;
